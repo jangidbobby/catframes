a049046374df2d1c50852b4ed1138af28902ce79
jangidbobby <111410518+jangidbobby@users.noreply.github.com>
2024-12-26 11:48:50 +0530

    Add files via upload

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Catframes',
+            'email' => 'contact@catframes.in',
+            'password' => bcrypt('Catframes@123'),
+        ]);
+    }
+}
